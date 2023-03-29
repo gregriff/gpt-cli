4a5af7eb75300f6f726e2024b64da99d98290a2b
Greg Griffin <greg@lapetussolutions.com>
2023-03-28 22:44:12 -0400

    added colors

--- api/main.py
+++ api/main.py
@@ -1,9 +1,10 @@
+import sys
 from typing import Generator
-import asyncio
 
 import openai
 
 from config import CONFIG
+from colors import *
 
 openai.api_key = CONFIG['apiKey2']
 model = 'gpt-3.5-turbo'
@@ -13,9 +14,12 @@ model = 'gpt-3.5-turbo'
 
 if __name__ == '__main__':
     while True:
+        sys.stdout.write(BOLD + CYAN)
         prompt = input('> ')
         if prompt in ('exit', 'e', 'q', 'quit'):
             exit(0)
+        sys.stdout.write(RESET)
+        sys.stdout.write(GREEN)
         response: Generator = openai.ChatCompletion.create(model=model, stream=True,
                                                            messages=[{'role': 'user', 'content': prompt}],
                                                            temperature=0.7)
@@ -24,3 +28,4 @@ if __name__ == '__main__':
             content: str = text_part.get('content', '')
             print(content, end='')
         print('\n')
+        sys.stdout.write(RESET)
