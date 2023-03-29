73355045ecd709db5f8d6c816fce98f2a98ddcc7
Greg Griffin <greg@lapetussolutions.com>
2023-03-28 23:26:34 -0400

    more colors

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 import sys
 from typing import Generator
+from importlib.metadata import version
 
 import openai
 
@@ -9,17 +10,32 @@ from colors import *
 openai.api_key = CONFIG['apiKey2']
 model = 'gpt-3.5-turbo'
 
+
+def greeting():
+    print()
+    sys.stdout.write(CYAN)
+    print('=*='*10)
+    sys.stdout.write(ORANGE)
+    print(f'{"openai" : <10}{"v"+version("openai") : <15}')
+    print(f'{"model" : <10}{model : <15}')
+    sys.stdout.write(RESET)
+    sys.stdout.write(CYAN)
+    print('=*='*10)
+    print('\n')
+
 # todo log and output to a file the sum of tokens used
 
 
 if __name__ == '__main__':
+    greeting()
     while True:
-        sys.stdout.write(BOLD + CYAN)
-        prompt = input('> ')
+        sys.stdout.write(BOLD + YELLOW)
+        prompt = input(f'? {CYAN}> ')
         if prompt in ('exit', 'e', 'q', 'quit'):
             exit(0)
         sys.stdout.write(RESET)
         sys.stdout.write(GREEN)
+        print()
         response: Generator = openai.ChatCompletion.create(model=model, stream=True,
                                                            messages=[{'role': 'user', 'content': prompt}],
                                                            temperature=0.7)
@@ -28,4 +44,3 @@ if __name__ == '__main__':
             content: str = text_part.get('content', '')
             print(content, end='')
         print('\n')
-        sys.stdout.write(RESET)
