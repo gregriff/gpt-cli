b94b9db3d81fa9a62280eb2fd1205167c6c9a5a4
Greg Griffin <greg@lapetussolutions.com>
2023-03-29 11:30:59 -0400

    system message, chat history and history clearing added

--- api/main.py
+++ api/main.py
@@ -10,7 +10,7 @@ from terminal import *
 max_text_width = max_text_width(get_terminal_size().columns)
 
 openai.api_key = CONFIG['lapetusAPIkey']
-model = 'gpt-3.5-turbo'
+model = 'gpt-4'
 
 # TODO:
 #   - implement chat functionality:
@@ -24,32 +24,39 @@ model = 'gpt-3.5-turbo'
 #     ]
 # )
 
-messages = [{'role': 'system',
-            'content': 'You are a concise assistant that helps software engineers'}]
-
+system_message = {'role': 'system', 'content': 'You are a concise assistant to software engineers'}
 
 if __name__ == '__main__':
     greeting(model)
+    messages = [system_message, ]
+    count = 0
     while True:
         try:
             prompt = input(f'{BOLD + YELLOW}? {CYAN}> ')
         except KeyboardInterrupt:
             exit(0)
-        if prompt in ('exit', 'e', 'q', 'quit'):
+        if prompt in ('exit', 'e', 'q', 'quit',):
             exit(0)
-        print(RESET, GREEN)
-        messages.append({'role': 'user', 'content': prompt})
-        response: Generator = openai.ChatCompletion.create(model=model, stream=True,
-                                                           messages=messages,
-                                                           temperature=0.7)
-        full_response = []
-        for chunk in response:
-            for choice in chunk['choices']:
-                text_part = choice['delta'].get('content', '')
-                full_response.append(text_part)
-                print(text_part, end='')
-            # text_part: dict = chunk['choices'][0]['delta']
-            # content: str = text_part.get('content', '')
-            # print(content, end='')
-        print('\n')
-        messages.append({"role": "assistant", "content": ''.join(full_response)})
+        if prompt in ('c', 'clear',):
+            print(RESET, BOLD, BLUE)
+            print(f'history cleared: {count} messages total', '\n', RESET)
+            messages, count = [system_message, ], 0
+            continue
+        else:  # prompt model
+            print(RESET, GREEN)
+            messages.append({'role': 'user', 'content': prompt})
+            response: Generator = openai.ChatCompletion.create(model=model, stream=True,
+                                                               messages=messages,
+                                                               temperature=0.7)
+            full_response = []
+            for chunk in response:
+                for choice in chunk['choices']:
+                    text_part = choice['delta'].get('content', '')
+                    full_response.append(text_part)
+                    print(text_part, end='')
+                # text_part: dict = chunk['choices'][0]['delta']
+                # content: str = text_part.get('content', '')
+                # print(content, end='')
+            print('\n')
+            messages.append({"role": "assistant", "content": ''.join(full_response)})
+            count += 1
