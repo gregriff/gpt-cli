efa1a95012d309a1b6c8814afc9f623331d8642a
Greg Griffin <greg@lapetussolutions.com>
2023-03-28 21:14:28 -0400

    implemented basic stdin while loop

--- api/main.py
+++ api/main.py
@@ -1,5 +1,20 @@
 import openai
+import requests
 from configuration.config import CONFIG
 
+openai.api_key = CONFIG['apiKey2']
+model = 'gpt-3.5-turbo'
+
+
+def parse_completion(compl: dict) -> str:
+    return compl['choices'][0]['message']['content']
+
+
 if __name__ == '__main__':
-    print(CONFIG['apiKey'])
+    while True:
+        prompt = input('> ')
+        if prompt in ('exit', 'e', 'q', 'quit'):
+            exit(0)
+        completion = openai.ChatCompletion.create(model=model, messages=[{'role': 'user', 'content': prompt}])
+        response = parse_completion(completion)
+        print(response)
