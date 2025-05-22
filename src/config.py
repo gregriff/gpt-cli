2651224ad482e7801be9b8331dd18ffa22f46fea
Greg Griffin <greg.griffin2@gmail.com>
2025-05-22 14:27:20 -0400

    add claude4sonnet+opus, make 4sonnet default

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ env_file = path.join(sys.path[0], "../env.json")
 with open(env_file) as file:
     CONFIG: dict = load(file)
 
-default_model = "claude-3-7-sonnet-latest"
+default_model = "claude-sonnet-4-20250514"
 default_system_message = "You are a concise assistant to a software engineer"
 default_max_tokens = 1024
 
@@ -43,12 +43,12 @@ MODELS_AND_PRICES: dict[str, dict[str, float | bool] | dict[str, float]] = {
             "prompt": 0.8 / 1_000_000,
             "response": 4.00 / 1_000_000,
         },
-        "claude-3-7-sonnet-latest": {
+        "claude-sonnet-4-20250514": {
             "prompt": 3.0 / 1_000_000,
             "response": 15.0 / 1_000_000,
             "thinking": True,
         },
-        "claude-3-opus-latest": {
+        "claude-opus-4-20250514": {
             "prompt": 15.0 / 1_000_000,
             "response": 75.0 / 1_000_000,
         },
